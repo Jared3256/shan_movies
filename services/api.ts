a9b1d6883774cb01614bcec6d533e1af6d85745b
Jared3256 <odhiambojared566@gmail.com>
2025-04-03 00:00:49 +0300

    set up useFetch hook

---
+++ services/api.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+export const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_KEY}`,
+  },
+};
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `/search/movie?query=${encodeURIComponent(query)}`
+    : "/discover/movie?sort_by=popularity.desc";
+
+  let data_response = undefined;
+
+  await axios
+    .get(TMDB_CONFIG.BASE_URL + endpoint, {
+      headers: TMDB_CONFIG.headers,
+    })
+    .then((response) => {
+      console.log(response.data);
+      data_response = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
