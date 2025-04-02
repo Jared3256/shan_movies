763ac8cf70070ba00d84a9b3fcc7dff80ae16e91
Jared3256 <odhiambojared566@gmail.com>
2025-04-02 22:37:13 +0300

    finished system routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
 import { Stack } from "expo-router";
-import "../styles/globals.css"
+import "../styles/globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="movie/[id]"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 }
