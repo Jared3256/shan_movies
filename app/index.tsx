56c70757488214f7f1e4beb7bc9c6695df920ed9
Jared3256 <odhiambojared566@gmail.com>
2025-04-02 21:39:27 +0300

    tailwind color-theme extended

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+     className="flex-1 justify-center items-center"
     >
-      <Text className="text-4xl">Edit app/index.tsxcvcv to edit this screen.</Text>
+      <Text className="text-3xl text-primary font-bold">Edit app/index.tsxcvcv to edit this screen.</Text>
     </View>
   );
 }
